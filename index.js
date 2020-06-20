095f1cd4246446b5d5fdadd568ff673a4bddacbc
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-06-20 05:42:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, obj);
   
+  delete newObj.key;
 }
 
