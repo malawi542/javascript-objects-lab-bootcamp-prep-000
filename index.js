81ed8d68cf53beca1e817eb121c13967ae524cbc
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-06-20 05:39:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, obj, {[key]: value});
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  obj[key] = value;
+  
+}
+
